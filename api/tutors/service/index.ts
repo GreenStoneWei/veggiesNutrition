b2f73e5f98922e624ff1cb4fb2037022c7c43137
stone.wei <stone.wei@wemoscooter.com>
2021-05-19 16:09:21 +0800

    interview review

--- api/tutors/service/index.ts
+++ api/tutors/service/index.ts
@@ -1,5 +1,11 @@
 import { LanguageSlug, CacheStrategy } from '../../../infra/enums/languages'
 import { TutorsStrategy, getCacheStrategy } from '../../factory'
+import redis from '../../../repositories/cache/redis'
+
+import { LockManager } from '../../../repositories/lock'
+
+const lockManager = new LockManager(redis.getClient())
+const TTL = 5
 
 export async function getTutorsBySlug(languageSlug: LanguageSlug) {
   const tutors = getCacheStrategy(CacheStrategy.tutors) as TutorsStrategy
@@ -9,7 +15,14 @@ export async function getTutorsBySlug(languageSlug: LanguageSlug) {
   let data
 
   if (!cache) {
+    const lock = await lockManager.acquireLock(languageSlug, TTL)
+    const cacheData = await tutors.checkIfCacheExists(languageSlug)
+    if (cacheData) {
+      await lockManager.releaseLock(lock)
+      return cacheData
+    }
     data = await tutors.refreshCache(languageSlug)
+    await lockManager.releaseLock(lock)
   } else {
     data = await tutors.getFromCache(cache, expirationDay, languageSlug)
   }
